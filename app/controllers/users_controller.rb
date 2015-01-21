be59fbf9cd098f564551ccfc3c3d9f5294317e46
nsranga <ranganadhan.nadadhur@gmail.com>
2015-01-22 00:50:39 +0530

    Completed login, logout, signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,14 @@ class UsersController < ApplicationController
   end
   
   def new
-    @user = User.new
+  	@user = User.new
+  	#render :layout => false
   end
   
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_url, notice: "Thank you for signing up!"
     else
       render "new"
