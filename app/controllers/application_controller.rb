e4e5c96357a5808ed0b5268b108b81ea9235f038
nsranga <ranganadhan.nadadhur@gmail.com>
2015-02-05 19:25:27 +0530

    private added in apps controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,8 @@ class ApplicationController < ActionController::Base
       params.require(:user).permit(:email, :password, :password_confirmation)
   end
 
+  private
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
